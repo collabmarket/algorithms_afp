d70652d61ee3c3e52e12bc68549ff43b1fc8368a
jrovegno <javier.rovegno@gmail.com>
2016-04-21 04:32:08 -0300

    Added Monetary Base Total

--- get_quandl.py
+++ get_quandl.py
@@ -2,12 +2,14 @@ import pandas_datareader.data as web
 import Quandl
 from datetime import datetime
 
-with open('token.txt', 'r') as f:
+with open('token.txt', 'rU') as f:
     token = f.readline().rstrip('\n')
+
 # Indicadores de Riesgo sistemico
 dow = Quandl.get('BCB/UDJIAD1', authtoken=token)
 wti = Quandl.get('EIA/PET_RWTC_D', authtoken=token)
 fedrate = web.DataReader("FEDFUNDS", 'fred', start=datetime(1955,1,1))
+money = web.DataReader("BOGMBASEW", 'fred', start=datetime(1976,1,1))
 # Riesgo de tipo de cambio (solo para mostrar el pobre uso de derivados en 2009)
 usdclp = Quandl.get("CURRFX/USDCLP", authtoken=token)
 
@@ -17,3 +19,4 @@ dow.to_csv(outdir + 'dow.csv')
 wti.to_csv(outdir + 'wti.csv')
 fedrate.to_csv(outdir + 'fedrate.csv')
 usdclp.to_csv(outdir + 'usdclp.csv')
+money.to_csv(outdir + 'money.csv')
