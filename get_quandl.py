d8c1ca64b3f93b581e8fce4b23e21838c75d42bf
jrovegno <javier.rovegno@gmail.com>
2016-04-22 19:10:26 -0300

    Added US debt data

--- get_quandl.py
+++ get_quandl.py
@@ -12,6 +12,7 @@ dow = Quandl.get('BCB/UDJIAD1', authtoken=token)
 wti = Quandl.get('EIA/PET_RWTC_D', authtoken=token)
 fedrate = web.DataReader("FEDFUNDS", 'fred', start=datetime(1955,1,1))
 money = web.DataReader("BOGMBASEW", 'fred', start=datetime(1976,1,1))
+debt = web.DataReader("GFDEGDQ188S", 'fred', start=datetime(1966,1,1))
 # Riesgo de tipo de cambio (solo para mostrar el pobre uso de derivados en 2009)
 usdclp = Quandl.get("CURRFX/USDCLP", authtoken=token)
 
@@ -22,5 +23,6 @@ wti.to_csv(outdir + 'wti.csv')
 fedrate.to_csv(outdir + 'fedrate.csv')
 usdclp.to_csv(outdir + 'usdclp.csv')
 money.to_csv(outdir + 'money.csv')
+debt.to_csv(outdir + 'debt.csv')
 
 print "[INFO]--" + datetime.now().strftime('%Y-%M-%d %H:%M:%S') + "--" + "get_quandl" + "--" + "DONE"
