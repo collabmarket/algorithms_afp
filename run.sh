1869e39780dd970b995ffde64c0fdd50c84c9df8
jrovegno <javier.rovegno@gmail.com>
2016-05-06 12:40:18 -0300

    Clean run tee and timeout

--- run.sh
+++ run.sh
@@ -5,17 +5,19 @@ export PATH=$HOME/miniconda/bin:$PATH
 cd $(dirname "$0")
 LOG_FILE="estrategia.log"
 DATE=$(date +%Y-%m-%d)
-
-git pull 2>&1 | tee -a ${LOG_FILE}
-python get_quandl.py 2>&1 | tee -a ${LOG_FILE}
-#jupyter nbconvert --to notebook --execute A-E_Capital.ipynb --output A-E_Capital.ipynb 2>&1 | tee -a ${LOG_FILE}
-jupyter nbconvert --ExecutePreprocessor.timeout=120 --to notebook --execute A-E_Cuprum.ipynb --output A-E_Cuprum.ipynb 2>&1 | tee -a ${LOG_FILE}
-jupyter nbconvert --ExecutePreprocessor.timeout=120 --to notebook --execute A-E_Habitat.ipynb --output A-E_Habitat.ipynb 2>&1 | tee -a ${LOG_FILE}
-jupyter nbconvert --ExecutePreprocessor.timeout=120 --to notebook --execute A-E_Modelo.ipynb --output A-E_Modelo.ipynb 2>&1 | tee -a ${LOG_FILE}
-#jupyter nbconvert --to notebook --execute A-E_Planvital.ipynb --output A-E_Planvital.ipynb 2>&1 | tee -a ${LOG_FILE}
-#jupyter nbconvert --to notebook --execute A-E_Provida.ipynb --output A-E_Provida.ipynb 2>&1 | tee -a ${LOG_FILE}
-jupyter nbconvert --ExecutePreprocessor.timeout=120 --to notebook --execute SystemicRisk.ipynb --output SystemicRisk.ipynb 2>&1 | tee -a ${LOG_FILE}
-git add result/*.png data_quandl/*.csv *.ipynb 2>&1 | tee -a ${LOG_FILE}
-git commit -m "Update data, nb and images $DATE" 2>&1 | tee -a ${LOG_FILE}
-git push 2>&1 | tee -a ${LOG_FILE}
-python tweet.py 2>&1 | tee -a ${LOG_FILE}
+TIMEOUT=300
+{
+git pull
+python get_quandl.py
+#jupyter nbconvert --to notebook --execute A-E_Capital.ipynb --output A-E_Capital.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=${TIMEOUT} --to notebook --execute A-E_Cuprum.ipynb --output A-E_Cuprum.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=${TIMEOUT} --to notebook --execute A-E_Habitat.ipynb --output A-E_Habitat.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=${TIMEOUT} --to notebook --execute A-E_Modelo.ipynb --output A-E_Modelo.ipynb
+#jupyter nbconvert --to notebook --execute A-E_Planvital.ipynb --output A-E_Planvital.ipynb
+#jupyter nbconvert --to notebook --execute A-E_Provida.ipynb --output A-E_Provida.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=${TIMEOUT} --to notebook --execute SystemicRisk.ipynb --output SystemicRisk.ipynb
+git add result/*.png data_quandl/*.csv *.ipynb
+git commit -m "Update data, nb and images $DATE"
+git push
+python tweet.py
+} 2>&1 | tee -a ${LOG_FILE}
