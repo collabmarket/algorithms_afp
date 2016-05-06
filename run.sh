da566fb81afae74945cdf367b828ecadb012533c
jrovegno <javier.rovegno@gmail.com>
2016-05-06 00:27:46 -0300

    Added timeout 120 nbconvert

--- run.sh
+++ run.sh
@@ -9,11 +9,12 @@ DATE=$(date +%Y-%m-%d)
 git pull 2>&1 | tee -a ${LOG_FILE}
 python get_quandl.py 2>&1 | tee -a ${LOG_FILE}
 #jupyter nbconvert --to notebook --execute A-E_Capital.ipynb --output A-E_Capital.ipynb 2>&1 | tee -a ${LOG_FILE}
-jupyter nbconvert --to notebook --execute A-E_Cuprum.ipynb --output A-E_Cuprum.ipynb 2>&1 | tee -a ${LOG_FILE}
-jupyter nbconvert --to notebook --execute A-E_Habitat.ipynb --output A-E_Habitat.ipynb 2>&1 | tee -a ${LOG_FILE}
-jupyter nbconvert --to notebook --execute A-E_Modelo.ipynb --output A-E_Modelo.ipynb 2>&1 | tee -a ${LOG_FILE}
+jupyter nbconvert --ExecutePreprocessor.timeout=120 --to notebook --execute A-E_Cuprum.ipynb --output A-E_Cuprum.ipynb 2>&1 | tee -a ${LOG_FILE}
+jupyter nbconvert --ExecutePreprocessor.timeout=120 --to notebook --execute A-E_Habitat.ipynb --output A-E_Habitat.ipynb 2>&1 | tee -a ${LOG_FILE}
+jupyter nbconvert --ExecutePreprocessor.timeout=120 --to notebook --execute A-E_Modelo.ipynb --output A-E_Modelo.ipynb 2>&1 | tee -a ${LOG_FILE}
 #jupyter nbconvert --to notebook --execute A-E_Planvital.ipynb --output A-E_Planvital.ipynb 2>&1 | tee -a ${LOG_FILE}
 #jupyter nbconvert --to notebook --execute A-E_Provida.ipynb --output A-E_Provida.ipynb 2>&1 | tee -a ${LOG_FILE}
+jupyter nbconvert --ExecutePreprocessor.timeout=120 --to notebook --execute SystemicRisk.ipynb --output SystemicRisk.ipynb 2>&1 | tee -a ${LOG_FILE}
 git add result/*.png data_quandl/*.csv *.ipynb 2>&1 | tee -a ${LOG_FILE}
 git commit -m "Update data, nb and images $DATE" 2>&1 | tee -a ${LOG_FILE}
 git push 2>&1 | tee -a ${LOG_FILE}
